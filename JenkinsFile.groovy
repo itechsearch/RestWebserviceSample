fa12ec84febf644c623d24913d1f6b7a8054862a
itechsearch <itechsearch@gmail.com>
2017-12-11 22:07:43 +0530

    Testing with pivotal cloud checking

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,25 +1,15 @@
-stage 'build'
-node {
-    git 'https://github.com/itechsearch/RestWebserviceSample' withEnv(["PATH+MAVEN=${tool 'm3'}/bin"]) {
-        bat "mvn -B –Dmaven.test.failure.ignore=true clean package"
+node('java8'){
+    stage('Configure'){
+        env.PATH="${tool 'maven-3.5.2'}/bin:${env.PATH}"
     }
-    stash excludes: 'target/', includes: '**', name: 'source'
-}
-stage 'test'
-parallel 'integration': {
-    node {
-        unstash 'source' withEnv(["PATH+MAVEN=${tool 'm3'}/bin"]) {
-            bat "mvn clean verify"
-        }
+    stage('Checkout'){
+        git "https://github.com/itechsearch/RestWebserviceSample"
     }
-}, 'quality': {
-    node {
-        unstash 'source' withEnv(["PATH+MAVEN=${tool 'm3'}/bin"]) {
-            bat "mvn sonar:sonar"
-        }
+    stage('Build'){
+        bat 'mvn -B -V -U -e clean package'
     }
-}
-stage 'approve'
-timeout(time: 3, unit: 'DAYS') {
-    input message: 'Do you want to deploy?', submitter: 'ops'
+    stage('Archive'){
+        junit allowEmptyResults: true, testResults:'**/target/**/TEST*.xml'
+
+    }
 }
