29f7c227906a6392f7c770e5aff1200c63381b02
itechsearch <itechsearch@gmail.com>
2017-12-12 22:02:44 +0530

    Testing with pivotal cloud checking

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -33,9 +33,9 @@ node {
                           usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
             bat '''
                
-                cf api https://api.run.pivotal.io
-                cf auth $USERNAME $PASSWORD
-                cf target -o ITechsearch -s ITechSearch
+                
+                cf login -a https://api.run.pivotal.io -u  $USERNAME -p $PASSWORD -o ITechsearch -s ITechSearch 
+                
                 cf push
                '''
         }
