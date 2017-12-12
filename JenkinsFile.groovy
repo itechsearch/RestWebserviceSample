1003aa79511617c3a9397f5566841baa1c455608
itechsearch <itechsearch@gmail.com>
2017-12-12 21:35:30 +0530

    Testing with pivotal cloud checking

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -29,8 +29,16 @@ node {
         // Depends on the 'Credentials Binding Plugin'
         // (https://wiki.jenkins-ci.org/display/JENKINS/Credentials+Binding+Plugin)
 
-        pushToCloudFoundry cloudSpace: 'ITechSearch', credentialsId: 'pivotalcf', organization: 'ITechSearch', target: 'https://api.run.pivotal.io'
+         withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: 'pivotalcf',
+                          usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            sh '''
+                curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 
-
+                ./cf api https://api.run.pivotal.io
+                ./cf auth $USERNAME $PASSWORD
+                ./cf target -o ITechsearch-demo -s development
+                ./cf push
+               '''
+        }
     }
 }
